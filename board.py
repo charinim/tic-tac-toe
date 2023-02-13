2eb8b257d2ac260f0d16467c312117a913a872d2
Harini Machiraju <116056136+charinim@users.noreply.github.com>
2023-02-13 15:12:18 -0800

    clean: board.py

--- board.py
+++ board.py
@@ -1,28 +1,16 @@
 class Board:
        def __init__(self):
-            # board is a list of cells that are represented 
-            # by strings (" ", "O", and "X")
-            # initially it is made of empty cells represented 
-            # by " " strings
             self.sign = " "
             self.size = 3
             self.board = list(" " * self.size**2)
-            #self.l1 = ["A1", "B1", "C1", "A2", "B2", "C2", "A3", "B3", "C3"]
-            #self.l2 = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-            # the winner's sign O or X
             self.winner = ""
+
        def get_size(self): 
-             # optional, return the board size (an instance size)
              return self.size
             
        def get_winner(self):
-            # return the winner's sign O or X (an instance winner)     
             return self.winner 
        def set(self, cell, sign):
-            # mark the cell on the board with the sign X or O
-            # you need to convert A1, B1, …, C3 cells into index values from 1 to 9
-            # you can use a tuple ("A1", "B1",...) to obtain indexes 
-            # this implementation is up to you
             labels = ["A1", "B1", "C1", "A2", "B2", "C2", "A3", "B3", "C3"]
             indices = [0,1,2,3,4,5,6,7,8]
 
@@ -33,9 +21,6 @@ class Board:
 
 
        def isempty(self, cell):
-            # you need to convert A1, B1, …, C3 cells into index values from 1 to 9
-            # return True if the cell is empty (not marked with X or O)
-
             labels = ["A1", "B1", "C1", "A2", "B2", "C2", "A3", "B3", "C3"]
             index = [0,1,2,3,4,5,6,7,8]
             try:
@@ -109,15 +94,12 @@ class Board:
             elif " " not in self.board:
                 self.winner = " "
                 done = True
-            
-            # check all game terminating conditions, if one of them is present, assign the var done to True
-            # depending on conditions assign the instance var winner to O or X
             return done
 
     
     
        def show(self):
-            # draw the board
+
             print("   A   B   C")
             print(" +---+---+---+")
             print(f"1| {self.board[0]} | {self.board[1]} | {self.board[2]} |")
